9c8adf7e95793a9ab219cf8d8be5e676f92d0b4c
williamparker <baiweilian@gmail.com>
2021-04-22 13:37:43 -0500

    Added mini-project templates

---
+++ matlab/mini_project.m
@@ -0,0 +1,19 @@
+%%%% RENAME from mini_project.m to (your_project_short_name).m
+% Comments describing mini-project ~ 100-200 words
+
+% Simulation parameters
+%	These are values particular to the simulation 
+%	that do not change later in the script
+
+% Computed parameters (from simulation parameters)
+%	These are values that do not change later in the script
+%	and are calculated from formulas using the simulation parameters
+
+% Function calls and simple calculations for:
+%	data read-in
+%	simulation solution 
+%	visualization
+
+% Function definitions for simulation solution & visualization
+%	Each function contains help text: https://www.mathworks.com/help/matlab/matlab_prog/add-help-for-your-program.html
+
---
+++ python/mini_project.py
@@ -0,0 +1,27 @@
+#### RENAME from mini_project.py to (your_project_short_name).py
+# File structure
+# 1. Commented paragraph describing mini-project ~ 100-200 words
+# 2. Module imports that are used in multiple functions
+# 3. Function definitions
+# 4. if __name__ == "__main__" block, which calls a primary function with a clear name 
+
+# All code is inside function definitions for simulation solution & visualization (functional programming)
+#	Each function contains a docstring compliant with PEP 257: https://www.python.org/dev/peps/pep-0257/
+#	Module ends with if __name__ == "__main__" block to execute central function of the code
+
+# Primary simulation function structure
+#	1. Module imports
+#		Use SciPy constants for physical constants in particular function (not globally)
+#			https://docs.scipy.org/doc/scipy/reference/constants.html
+#		Follow best practice order: 
+#			https://docs.python.org/3/faq/programming.html#what-are-the-best-practices-for-using-import-in-a-module
+# 	2. Simulation parameters
+#		Each parameter named clearly and units marked in in-line comment
+#		Naming of all variables should comply with PEP 8: 
+#			https://www.python.org/dev/peps/pep-0008/#documentation-strings
+#			(lower_case_with_underscores)
+# 	3. Computed parameters (from simulation parameters)
+# 	4. Function calls (use PEP 8-compliant lower_case_with_underscores) and simple calculations for:
+#		data read-in
+#		simulation solution 
+#		visualization
