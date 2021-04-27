696e5bae40b955329bbc43b52b303713080c38a5
williamparker <baiweilian@gmail.com>
2021-04-27 09:13:14 -0500

    Added reviewer comments to mini-project template

--- matlab/mini_project.m
+++ matlab/mini_project.m
@@ -17,3 +17,13 @@
 % Function definitions for simulation solution & visualization
 %	Each function contains help text: https://www.mathworks.com/help/matlab/matlab_prog/add-help-for-your-program.html
 
+%%%% Reviewer comments
+% code gives Unrecognized function or variable 'calculate_currents'
+% code reads very well and varibles are well defined
+% code comments are great, and provide support for the well defined
+% varibles
+%varibles are well defined and are in line with the problem solved
+%the range of varibles used defines the problem well
+%Given the explicit nature of the varibles I can visulize whats going on
+
+%code might need semicolons to suppress out put on some lines
