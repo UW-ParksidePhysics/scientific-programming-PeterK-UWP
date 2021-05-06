0363980b30f42d0ce1e0aa5d09a3a7f4fe2d9e02
Peter Kveton <>
2021-05-06 17:35:01 +0000

    fixed fitcurve

--- final_stuff/fit_curve_array.py
+++ final_stuff/fit_curve_array.py
@@ -1,8 +1,16 @@
-import numpy as numpy
+import numpy as np
 
 
 def fit_curve_array(quadratic_coefficients, min_x, max_x, number_of_points=100):
+
   if max_x < min_x:   #max values cannot be less than min values
     raise ArithmeticError
   if number_of_points <= 2:   #need more than 2 points
     raise IndexError
+
+  x_array = np.linspace(min_x, max_x, number_of_points)
+  y_array = np.polyval(quadratic_coefficients, x_array)
+  data = np.array(x_array, y_array)
+
+  return data
+
