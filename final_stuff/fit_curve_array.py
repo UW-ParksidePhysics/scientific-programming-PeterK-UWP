d6935cebf86bb699a10ecdb5728061c389b838c2
PeterKveton <>
2021-05-06 02:16:51 +0000

    added comment

--- final_stuff/fit_curve_array.py
+++ final_stuff/fit_curve_array.py
@@ -2,7 +2,7 @@ import numpy as numpy
 
 
 def fit_curve_array(quadratic_coefficients, min_x, max_x, number_of_points=100):
-  if max_x < min_x:
+  if max_x < min_x:   #max values cannot be less than min values
     raise ArithmeticError
-  if number_of_points <= 2:
+  if number_of_points <= 2:   #need more than 2 points
     raise IndexError
