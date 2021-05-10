efec3fe1e97490cab045f5b5f81d283fc73b9493
Peter Kveton <>
2021-05-10 19:59:58 +0000

    save

--- final_stuff/final_project.py
+++ final_stuff/final_project.py
@@ -1,14 +1,36 @@
 import matplotlib.pyplot as plt
 
 from two_column_text_read import two_column_text_read
-from quadratic_fit import quadratic_fit
-from fit_curve_array import fit_curve_array
+array = two_column_text_read("volumes_energies.dat")
+#print(array)
+
 from bivariate_statistics import bivariate_statistics
+statistics = bivariate_statistics(array)
+print(statistics)
+min_x = statistics[2]
+max_x = statistics[3]
+print(min_x)
+print(max_x)
+
+from quadratic_fit import quadratic_fit
+quadratic_coefficients = quadratic_fit(array)
+#print(quadratic_coefficients)
+
+from fit_curve_array import fit_curve_array
+data = fit_curve_array(quadratic_coefficients, min_x, max_x)
+plt.plotshow()
+
 from plot_data_with_fit import plot_data_with_fit
 
-array = two_column_text_read("volumes_energies.dat")
+from lowest_eigenvectors import lowest_eigenvectors
+
+
+
+
+
+
 #plt.show(plot_data_with_fit(array, fit_curve_array))
-print(fit_curve_array(quadratic_fit(array), 0, 10,))
-print(bivariate_statistics(array))
+#print(fit_curve_array(quadratic_fit(array), 0, 10,))
+#print(bivariate_statistics(array))
 
 #def fit_curve_array(quadratic_coefficients, min_x, max_x, number_of_points=100):
--- final_stuff/plot_data_with_fit.py
+++ final_stuff/plot_data_with_fit.py
@@ -3,8 +3,10 @@
 import matplotlib.pyplot as plt
 
 def plot_data_with_fit(data, fit_curve, data_format="", fit_format=""):
+  
   scatter_plot = plt.plot(data[0, :], data[1, :], data_format)
   curve_plot = plt.plot(fit_curve[0, :], fit_curve[1, :], fit_format)
   plt.show()
   return scatter_plot, curve_plot
+
 
