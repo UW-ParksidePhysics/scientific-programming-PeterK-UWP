fc3f78ec3bfbe5a1e4e265d47402d79b1e9ff032
Peter Kveton <78393553+PeterK-UWP@users.noreply.github.com>
2021-05-10 19:13:29 -0500

    Update plot_data_with_fit.py

--- final_stuff/plot_data_with_fit.py
+++ final_stuff/plot_data_with_fit.py
@@ -1,4 +1,8 @@
-
+"""
+This function take in an array of data and another array of many small points of data
+and plots a curves for each of the class
+__author__ = "Peter & Lena"
+"""
 
 import matplotlib.pyplot as plt
 
@@ -8,5 +12,3 @@ def plot_data_with_fit(data, fit_curve, data_format="", fit_format=""):
   curve_plot = plt.plot(fit_curve[0, :], fit_curve[1, :], fit_format)
   plt.show()
   return scatter_plot, curve_plot
-
-
