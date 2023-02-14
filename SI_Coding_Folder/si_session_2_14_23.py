76cb7b6893bcd509da7bbf469058f43ca96d4367
PeterK <kveto002@rangers.uwp.edu>
2023-02-14 08:54:19 -0600

    si session stuff

--- SI_Coding_Folder/si_session_2_14_23.py
+++ SI_Coding_Folder/si_session_2_14_23.py
@@ -9,19 +9,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # for loop
-n = eval(input('input number:'))
-array = np.arange(0, n + 1)  # x values
-list_of_numbers = []  # y values
-for i in array:
-    y = i ** 2  # y = x^2
-    list_of_numbers.append(y)
-print(list_of_numbers)
+n = eval(input('input number:'))     # this will prompt you with a max x value for the graph
+array = np.arange(0, n + 1)          # x values (need n above to work)
+list_of_numbers = []                 # y values,  empty list that we will append values to
+for i in array:                      # for loop, 'i' can be any variable/word (whatever you want)
+    y = i ** 2                       # y = x^2, function to loop over with our x values
+    list_of_numbers.append(y)        # appending y values to list
+print(list_of_numbers)               # not necessary but shows our code works
 
 # plotting, (need these loops above to make plots)
-plt.plot(array, list_of_numbers, color='red', label='y = x^2')
-plt.xlabel('x values')
-plt.ylabel('y values')
-plt.legend(["y = x^2"], loc="lower right")
-plt.title('graph of y = x^2')
-plt.savefig('y = x^2')
-plt.show()
+plt.plot(array, list_of_numbers, color='red', label='y = x^2')     # main plot(x values, y values, color, label)
+plt.xlabel('x values')                                             # x label
+plt.ylabel('y values')                                             # y label
+plt.legend(["y = x^2"], loc="lower right")                         # legend (key)
+plt.title('graph of y = x^2')                                      # title of graph
+plt.savefig('y = x^2')                                             # saves graph as a png
+plt.show()                                                         # display the graph
